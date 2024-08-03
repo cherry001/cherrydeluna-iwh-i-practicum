91a0f4632e47e9ae998a9c2b623bbceb17c944df
cherry001 <5403729+cherry001@users.noreply.github.com>
2024-08-04 00:08:51 +0800

    create route for the root page

--- index.js
+++ index.js
@@ -1,71 +1,82 @@
+require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
-const app = express();
+const bodyParser = require('body-parser');
+const hubspot = require('@hubspot/api-client');
 
+const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PORT = process.env.PORT || 3000;
+const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+const HUBSPOT_PROJECT_OBJECT_TYPE = process.env.HUBSPOT_PROJECT_OBJECT_TYPE;
+
+const hubspotClient = new hubspot.Client({ accessToken: HUBSPOT_API_KEY });
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+  try {
+    const apiResponse = await hubspotClient.crm.objects.basicApi.getPage(
+      HUBSPOT_PROJECT_OBJECT_TYPE,
+      10,
+      undefined,
+      ['name', 'description', 'status'],
+      undefined,
+      ['contacts'],
+      false
+    );
 
-// * Code for Route 1 goes here
+    const projectsWithContacts = apiResponse.results.map(async (project) => {
+      const contactCount =
+        project.associations && project.associations.contacts
+          ? project.associations.contacts.results.length
+          : 0;
+
+      let contacts = [];
+
+      if (contactCount > 0) {
+        const contactIds = project.associations.contacts.results.map(
+          (assoc) => assoc.id
+        );
+
+        const contactDetails = await hubspotClient.crm.contacts.batchApi.read({
+          inputs: contactIds.map((id) => ({ id })),
+          properties: ['firstname', 'lastname'],
+        });
+
+        contacts = contactDetails.results.map((contact) => ({
+          id: contact.id,
+          firstname: contact.properties.firstname,
+          lastname: contact.properties.lastname,
+        }));
+      }
+
+      return {
+        project,
+        contacts,
+      };
+    });
+
+    const projects = await Promise.all(projectsWithContacts);
+    res.render('index', {
+      projects,
+    });
+  } catch (e) {
+    console.error(
+      e.message === 'HTTP request failed'
+        ? JSON.stringify(e.response, null, 2)
+        : e
+    );
+    res.status(500).send('Error retrieving projects and contacts');
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
-
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
