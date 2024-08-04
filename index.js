39278da768c86d596e9bcd2625ebbb22eef8a496
cherry001 <5403729+cherry001@users.noreply.github.com>
2024-08-04 12:20:28 +0800

    create update project details page

--- index.js
+++ index.js
@@ -75,6 +75,26 @@ app.get('/', async (req, res) => {
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update-cobj/:id', async (req, res) => {
+  const projectId = req.params.id;
+
+  try {
+    const projectResponse = await hubspotClient.crm.objects.basicApi.getById(
+      HUBSPOT_PROJECT_OBJECT_TYPE,
+      projectId,
+      ['name', 'description', 'status']
+    );
+
+    res.render('update', { project: projectResponse });
+  } catch (e) {
+    console.error(
+      e.message === 'HTTP request failed'
+        ? JSON.stringify(e.response, null, 2)
+        : e
+    );
+    res.status(500).send('Error retrieving project');
+  }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
